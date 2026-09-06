14f4f7a86a37d862300a7de3b7abef38206b25dd
Anish Agarwal <34007888+thisisanish@users.noreply.github.com>
2026-09-06 14:17:12 +0530

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.ts
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums: number[], k: number): number[] {
+        let hashy:Record<number,number> =  {}
+        for(let i = 0; i<nums.length; i++){
+            if(!hashy[nums[i]]) hashy[nums[i]] = 1
+            else hashy[nums[i]]++
+        }
+        console.log(Object.entries(hashy).sort((a,b)=>b[1]-a[1]))
+       return Object.entries(hashy).sort((a,b)=>b[1]-a[1]).slice(0,k).map(a=> Number(a[0]))
+    }
+}
